b8d0890abe524227c7d95df23ca65b5a56668236
Chan Broset <bdchanbroset@gmail.com>
2021-02-17 13:25:58 +0000

    Update ex7

--- ex7.py
+++ ex7.py
@@ -9,7 +9,6 @@ D2 = {'Kim', 'Tom', 'Neil', 'Kramer', 'Dan', 'Jimmy', 'Teresa'}
 
 # Combined set
 D3 = D1.union(D2)
-
 printLine()
 # Print Sets
 print("Set 1:", D1)
@@ -29,5 +28,13 @@ printLine()
 print("Unique members in D1\n" + str([ele for ele in D1 if ele not in D2]))
 printLine()
 
-print("List of all members from both D1 and D2\n" + str([ele for ele in D3]))
+print("List of all members with no duplicate\n" + str([ele for ele in D3]))
+listSet = list(com_set)
+listSet.extend(list(D3))
+
+print("\nList of both sets enabled duplicate record:")
+print(listSet)
 printLine()
+
+
+# I'm not sure about list down all the members in D1 and D2, does it mean allow
